59eccab3349e05674ecb02949d8189d9b47e576c
SON <son@SONs-Mac-mini.local>
2022-07-14 10:52:15 +0700

    init app

---
+++ Hoc_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hoc_IOS
+//
+//  Created by SON on 14/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
